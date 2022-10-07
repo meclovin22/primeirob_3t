c394e93036b31c398447b16f855224366545bcc3
guilhermeaugusto <110115887+meclovin22@users.noreply.github.com>
2022-10-07 09:06:52 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -31,3 +31,18 @@ function multiplica(){
         }
     }
 }
+
+function total(){
+    let v = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let r = 0;
+    for(let i=1; i <=t;i++){
+     r = v * (1+ (j/100));
+     document.write
+     v = r;
+    }
+   
+
+    document.write("resultado:" + r);
+}
