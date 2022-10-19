c13c63972e491067f42715174eb10e9281d9e487
guilhermeaugusto <110115887+meclovin22@users.noreply.github.com>
2022-10-19 12:38:52 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -46,3 +46,13 @@ function total(){
 
     document.write("resultado:" + r);
 }
+function media(){
+    let n1= document.getElementById("n1").value;
+    let n2= document.getElementById("n2").value;
+    let n3= document.getElementById("n3").value;
+    let n4= document.getElementById("n4").value;
+
+      let r= (Number(n1)+Number(n2)+Number(n3)+Number(n4))/4;
+
+      document.getElementById("resul").innerHTML  = "Média: " + r;
+}
