e7eb19aa80885020edd5ab1bfe151371dd9a35c7
guilhermeaugusto <110115887+meclovin22@users.noreply.github.com>
2022-10-22 00:07:22 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,58 +1,113 @@
-
-let tabuada = 69
+let tabuada = 111;
 
 function escreva(){
-document.write( "<h1> tabuada do " + tabuada + "<br>"  )
-    document.write(tabuada + " x1 = " + (tabuada*1) +"<br>" );
-    document.write(tabuada + " x2 = " + (tabuada*2) + "<br>" )
-    document.write(tabuada + " x3 = " + (tabuada*3) +"<br>" )
-    document.write(tabuada + " x4 = " + (tabuada*4) +"<br>" )
-    document.write(tabuada + " x5 = " + (tabuada*5) +"<br>" )
-    document.write(tabuada + " x6 = " + (tabuada*6) +"<br>" )
-    document.write(tabuada + " x7 = " + (tabuada*7) +"<br>" )
-    document.write(tabuada + " x8 = " + (tabuada*8) +"<br>" )
-    document.write(tabuada + " x9 = " + (tabuada*9) +"<br>" )
-    document.write(tabuada + " x10 = " + (tabuada*10) +"<br>" )
+document.write(" Tabuada do " + tabuada + "<br>");    
+document.write(tabuada + " X 1 = " + (tabuada*1) + "<br>");
+document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
+document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
+document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
+document.write(tabuada + " x 5 = " + (tabuada*5) + "<br>");
+document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>");
+document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>");
+document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
+document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
+document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
 }
 
-let lista = [ "jesus", "spessato" , "duarte" , "jorge" ]
+let lista = ["gilzinho","spessato","neuza","jesus","Duarte"];
 
 function mostraLista(){
-    document.write("tamanho da lista:" + lista.length + "<br>")
-    for(let i = 0; i <lista.length;  i++ ){
-        document.write("Professor: " + lista[i] + "<br>");
+    document.write("tamanho da lista: "  +  lista.length + "<br>");
+    for(let i = 0; i <lista.length; i++){
+        document.write("professores: " + lista[i] + "<br>")
     }
 }
-function multiplica(){
-    for(let i = 1; i <= 10; i++){
-        document.write("o valor do i = " +i+ "<br>")
-        for(let j = 1; j <= 10; j++){
-            document.write(i + " x "+j+" = " + (i*j) + "<br>")
-        }
-    }
+
+function multiplica (){
+      for(let i = 1; i <= 10; i++){
+      document.write("Taboada do = " + i + "<br>");
+      for(let j = 1; j <=10; j++){
+        document.write(i + " x"+j+ " = " + (i*j) + "<br>");
+
+      }
+      document.write("<br>")
+  }
+}
+function moeda(atual){
+  return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+
 }
 
 function total(){
-    let v = document.getElementById("valor").value;
-    let j = document.getElementById("juros").value;
-    let t = document.getElementById("meses").value;
-    let r = 0;
-    for(let i=1; i <=t;i++){
-     r = v * (1+ (j/100));
-     document.write
-     v = r;
-    }
-   
+  let  v = document.getElementById("Valor").value;
+  let  j = document.getElementById("Juros").value;
+  let  t = document.getElementById("Meses").value; 
 
-    document.write("resultado:" + r);
+if(!Number(v)){
+ alert("O valor deve ser numérico.");
+ document.getElementById("Valor").value = "";
+ document.getElementById("Valor").focus();
+  return
+}
+if(!Number(j)){
+  alert("O juros deve ser numérico.");
+  document.getElementById("Juros").value = "";
+  document.getElementById("Juros").focus();
+   return
+ }
+
+ if(!Number(t)){
+  alert("O valor dos meses deve ser numérico.");
+  document.getElementById("Meses").value = "";
+  document.getElementById("Meses").focus();
+   return
+ }
+ 
+  let  r = 0;
+for (let i=1; i <= t;i++) {
+  r = v * (1+(j/100));
+  document.write("Mês "+ i + "valor: " + moeda(r) + "<br>");
+  v = r; 
+
+}
+  document.write("Resultado: "+ moeda(r));
 }
 function media(){
-    let n1= document.getElementById("n1").value;
-    let n2= document.getElementById("n2").value;
-    let n3= document.getElementById("n3").value;
-    let n4= document.getElementById("n4").value;
+  let n1 = document.getElementById("n1").value;
+  let n2 = document.getElementById("n2").value;
+  let n3 = document.getElementById("n3").value;
+  let n4 = document.getElementById("n4").value;
 
-      let r= (Number(n1)+Number(n2)+Number(n3)+Number(n4))/4;
+let r = (Number (n1)+ Number(n2)+ Number(n3)+ Number(n4))/4;
+document.getElementById("resul").innerHTML ="Media:" + r;
+}
 
-      document.getElementById("resul").innerHTML  = "Média: " + r;
+function somar(){
+  let n1 = document.getElementById("n1").value;
+  let n2 = document.getElementById("n2").value;
+  let n3 = document.getElementById("n3").value;
+  let n4 = document.getElementById("n4").value;
+
+let r = (Number (n1)+ Number(n2)+ Number(n3)+ Number(n4));
+document.getElementById("resul").innerHTML ="Soma:" + r;
+}
+
+function diminuir (){
+  let n1 = document.getElementById("n1").value;
+  let n2 = document.getElementById("n2").value;
+  let n3 = document.getElementById("n3").value;
+  let n4 = document.getElementById("n4").value;
+
+let r = (Number (n1)- Number(n2)- Number(n3)- Number(n4));
+document.getElementById("resul").innerHTML ="Diminuição:" + r;
+}
+
+function multiplicar (){
+  let n1 = document.getElementById("n1").value;
+  let n2 = document.getElementById("n2").value;
+  let n3 = document.getElementById("n3").value;
+  let n4 = document.getElementById("n4").value;
+
+let r = (Number (n1)* Number(n2)* Number(n3)* Number(n4));
+document.getElementById("resul").innerHTML ="Multiplicação:" + r;
 }
